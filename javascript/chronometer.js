1e5056913646b745bb2b908da6ce02e7baf42a8e
gunnerag <gunner.andersen.gil@gmail.com>
2020-07-20 08:40:30 +0200

    chronometer

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,15 +1,24 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0; 
   }
   startClick(callback) {
     // ... your code goes here
+    setInterval(()=>{ 
+      this.currentTime=this.currentTime+1;
+      }, 1000);
   }
   getMinutes() {
     // ... your code goes here
+    let minutes = Math.floor(Number((this.currentTime/60)))
+    return minutes
   }
   getSeconds() {
+    let seconds = (this.currentTime-this.getMinutes()*60)
+    return seconds
     // ... your code goes here
+    
   }
   twoDigitsNumber() {
     // ... your code goes here
